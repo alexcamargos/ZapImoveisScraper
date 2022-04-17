b6b8d0d174e1bfc0ea1a0521a64d1b66fcb26d7a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-04-17 17:41:08 -0300

    Code reorganized into subroutines.

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Name: scraper.py
-# Version: 0.0.2
+# Version: 0.1.3
 # Summary: Zap Imóveis Scraper
 #          A scraper that gathers data from Zap Imóveis website using BeautifulSoup.
 #
@@ -19,7 +19,9 @@ Zap Imóveis Scraper
 A scraper that gathers data from Zap Imóveis website using BeautifulSoup.
 """
 
+import json
 import logging
+import time
 from enum import Enum
 from random import randint
 
@@ -38,33 +40,58 @@ PATH = '/%(action)s/%(unit_type)s/%(state)s+%(city)s/?pagina=%(page)s'
 
 PORT = 443
 CERT_REQS = 'CERT_NONE'
-QUANTITY_TO_FETCH = 3
+QUANTITY_TO_FETCH = 5
+RANDINT_STARTING = 60
+RANDINT_FINAL = 120
 
 
 class BusinessFilter(Enum):
+    """Filters groups enumeration."""
+
     Comprar = 'Venda'
     Alugar = 'Aluguel'
     Lancamentos = 'Lançamentos'
 
 
 class UnitType(Enum):
+    """Unit type filter enumeration."""
+
     Todos = 'Imoveis'
     Casas = 'Casas'
     Apartamentos = 'Apartamentos'
     Quitinetes = 'Quitinetes'
 
 
+class ListedItem:
+    """A listed item on zapimoveis.com.br."""
+
+    Price = None
+    Condominium_fee = None
+    IPTU_fee = None
+    Floor_size = None
+    Number_bedrooms = None
+    Number_bathrooms = None
+    Parking_spaces = None
+    Address = None
+    Title = None
+    Description = None
+    Link = None
+    Publisher = None
+    Item_ID = None
+    Unit_types = None
+
+
 class DataScraper:
+    """Extracting selected data from the site zapimoveis.com.br and save for analysis."""
 
     def __init__(self, *args):
+        """Initiating an empty DataScraper."""
+
         super(DataScraper, self).__init__(*args)
 
     @staticmethod
-    def get_data(action, unit_type, city, page):
-        """Fetch data on the zapimoveis.com.br website using selection criteria.
-
-        Return all DIVs with card-container class.
-        """
+    def __fetch_data(action, unit_type, city, page):
+        """Fetch data on the zapimoveis.com.br website using selection criteria."""
 
         # Constructing the query using selection criteria.
         query_path = PATH % ({'action': slugify(action),
@@ -74,138 +101,115 @@ class DataScraper:
                               'page': page})
 
         pool = urllib3.HTTPSConnectionPool(DOMAIN_NAME, PORT, CERT_REQS)
+
         # Make a request.
         page = pool.request('GET', query_path)
 
         logging.info(f'GET >> {pool.host}{query_path}\tSTATUS: {page.status}')
 
         soup = BeautifulSoup(page.data.decode('utf-8'), 'html.parser')
-        page_elements = soup.find_all('div', {'class': 'card-container'})
 
-        return page_elements
+        page_data = soup.find(lambda tag: tag.name == "script"
+                                          and isinstance(tag.string, str)
+                                          and tag.string.startswith("window"))
 
-    def __data_to_csv(self):
-        # http = urllib3.PoolManager()
+        json_string = page_data.string.replace("window.__INITIAL_STATE__=", "").replace(
+            ";(function(){var s;(s=document.currentScript||document.scripts["
+            "document.scripts.length-1]).parentNode.removeChild(s);}());",
+            "")
 
-        valor = []
-        tipo_alugel = []
-        iptu = []
-        area = []
-        quartos = []
-        banheiros = []
-        vagas = []
-        uri = []
-        cidade = []
-        logradouro = []
-        bairro = []
-        tipo_negociacao = []
+        return json.loads(json_string)['results']['listings']
 
-        for query_cidade in TOWNS:
-            for tipo_imovel in UnitType:
-                for page in range(1, QUANTITY_TO_FETCH):
-                    sleep_time = randint(60, 120)
+    @staticmethod
+    def __data_scraper(data):
+        """Processing the data obtained from the zapimoveis.com.br website."""
 
-                    # Get all DIVs with card-container class.
-                    result = self.get_data(BusinessFilter.Alugar.value, tipo_imovel.value, query_cidade, page)
+        item = ListedItem()
 
-                    for row in result:
-                        tipo_negociacao.append(tipo_imovel.value)
-                        cidade.append(query_cidade)
+        item.Price = float(data['listing']['pricingInfos'][0].get('price', 0) if len(
+            data['listing']['pricingInfos']) > 0 else 0)
+        item.Condominium_fee = float(data['listing']['pricingInfos'][0].get('monthlyCondoFee', 0) if len(
+            data['listing']['pricingInfos']) > 0 else 0)
+        item.IPTU_fee = float(data['listing']['pricingInfos'][0].get('yearlyIptu', 0) if len(
+            data['listing']['pricingInfos']) > 0 else 0)
+        item.Floor_size = float(data['listing']['usableAreas'][0] if len(data['listing']['usableAreas']) > 0 else 0)
+        item.Number_bedrooms = int(data['listing']['bedrooms'][0] if len(data['listing']['bedrooms']) > 0 else 0)
+        item.Number_bathrooms = int(data['listing']['bathrooms'][0] if len(data['listing']['bathrooms']) > 0 else 0)
+        item.Parking_spaces = int(
+            data['listing']['parkingSpaces'][0] if len(data['listing']['parkingSpaces']) > 0 else 0)
+        item.Address = (data['link']['data']['street'] + ", " + data['link']['data']['neighborhood'] + ", " +
+                        data['link']['data']['state']).strip(',').strip()
+        item.Title = data['listing']['title']
+        item.Description = data['listing']['description'].replace('<br>', '\n').strip()
+        item.Link = data['link']['href']
+        item.Publisher = data['account']['name']
+        item.Item_ID = data['listing']['id']
+        item.Unit_types = data['listing']['unitTypes']
 
-                        endereco_find = row.find('h2', 'simple-card__address color-dark text-regular')
-                        endereco_text = ''
+        return item
 
-                        if endereco_find:
-                            endereco_text = endereco_find.text
-                            endereco_text = endereco_text.strip()
-
-                        if ',' in endereco_text:
-                            logradouro_tmp, bairro_tmp = endereco_text.split(',')
-                            logradouro.append(logradouro_tmp)
-                            bairro.append(bairro_tmp)
-                        else:
-                            logradouro.append(endereco_text)
-                            bairro.append('')
-
-                        valor_find = row.find("p",
-                                              "simple-card__price js-price color-darker heading-regular "
-                                              "heading-regular__bolder align-left")
-                        if valor_find:
-                            valor_find = valor_find.text.split('\n')
-                            valor.append(valor_find[1].replace('.', '').replace(' ', '').replace('R$', ''))
-                            tipo_alugel.append(valor_find[2].replace('/', ''))
-                        else:
-                            valor.append(0)
-                            tipo_alugel.append('')
-
-                        iptu_find = row.find('span', 'card-price__value')
-
-                        if iptu_find:
-                            iptu.append(iptu_find.text.replace('R$', '').replace('.', ''))
-                        else:
-                            iptu.append(0)
-
-                        area_find = row.find_all('li', {'class': 'feature__item text-small js-areas'})
-                        area_text = 0
-
-                        for li in area_find:
-                            area_find_span = li.find_all('span')[1]
-                            area_text = area_find_span.text
-                            area_text = area_text.replace('m²', '').replace(' ', '')
-
-                        area.append(area_text.strip())
-
-                        quatros_find = row.find_all('li', 'feature__item text-small js-bedrooms')
-                        quartos_text = 0
-
-                        for li in quatros_find:
-                            quatros_find_span = li.find_all('span')[1]
-                            quartos_text = quatros_find_span.text
-                            quartos_text = quartos_text.strip()
-
-                        quartos.append(quartos_text)
-
-                        vagas_find = row.find_all('li', 'feature__item text-small js-parking-spaces')
-
-                        vagas_text = 0
-
-                        for li in vagas_find:
-                            vagas_find_span = li.find_all('span')[1]
-                            vagas_text = vagas_find_span.text
-                            vagas_text = vagas_text.strip()
-
-                        vagas.append(vagas_text)
-
-                        banheiros_find = row.find_all('li', 'feature__item text-small js-bathrooms')
-
-                        banheiros_text = 0
-
-                        for li in banheiros_find:
-                            banheiros_text_span = li.find_all('span')[1]
-                            banheiros_text = banheiros_text_span.text
-                            banheiros_text = banheiros_text.strip()
-
-                        banheiros.append(banheiros_text)
-        # End scrapper
+    @staticmethod
+    def __data_to_csv(data):
+        """Write data to a comma-separated values (csv) file."""
 
         # Data frame constructor.
-        data_frame = pd.DataFrame(valor, columns=['Valor'])
-        data_frame['Area'] = area
-        data_frame['Quartos'] = quartos
-        data_frame['Banheiros'] = banheiros
-        data_frame['Vagas'] = vagas
-        data_frame['TipoNegociacao'] = tipo_negociacao
-        data_frame['TipoAluguel'] = tipo_alugel
-        data_frame['Iptu'] = iptu
-        data_frame['Logradouro'] = logradouro
-        data_frame['Bairro'] = bairro
-        data_frame['Cidade'] = cidade
+        data_frame = pd.DataFrame([(item.Price,
+                                    item.Condominium_fee,
+                                    item.Floor_size,
+                                    item.Number_bedrooms,
+                                    item.Number_bathrooms,
+                                    item.Parking_spaces,
+                                    item.IPTU_fee,
+                                    item.Address,
+                                    item.Title,
+                                    # item.Description,
+                                    item.Link,
+                                    item.Publisher,
+                                    item.Item_ID,
+                                    item.Unit_types) for item in [item for item in data]],
+                                  columns=['Price',
+                                           'Condominium',
+                                           'FloorSize',
+                                           'NumberOfBedrooms',
+                                           'NumberOfBathrooms',
+                                           'ParkingSpaces',
+                                           'IPTU',
+                                           'Address',
+                                           'Title',
+                                           # TODO: Realizar a limpesa das informaçãoes antes de salvar no data frame.
+                                           # 'Description',
+                                           'Link',
+                                           'Publisher',
+                                           'ItemID',
+                                           'UnitTypes'])
 
         # Write data frame to a comma-separated values (csv) file.
         data_frame.to_csv('data.csv', index=False)
 
+    def __get_data(self):
+        """Processing the pagination of results."""
+
+        listed_items = []
+
+        for query_town in TOWNS:
+            for unit_type in UnitType:
+                for page in range(1, QUANTITY_TO_FETCH):
+                    sleep_time = randint(RANDINT_STARTING, RANDINT_FINAL)
+
+                    results = self.__fetch_data(BusinessFilter.Alugar.value, unit_type.value, query_town, page)
+
+                    for result in results:
+                        listed_items.append(self.__data_scraper(result))
+
+                    logging.info(f'Delay execution: {sleep_time}')
+                    time.sleep(sleep_time)
+
+        return listed_items
+
     def execute(self):
-        self.__data_to_csv()
+        """Write data to a comma-separated values (csv) file."""
+
+        self.__data_to_csv(self.__get_data())
 
 
 def main():
